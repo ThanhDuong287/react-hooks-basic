be50a77afeb2cf53cc593b874806e364630cbb74
ThanhDuong287 <thanhduong28720@gmail.com>
2022-06-22 11:46:27 +0700

    To Do List

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.scss';
 import ColorBox from './components/ColorBox';
+import ToDoList from './components/ToDoList';
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: 'Marcelo Vieira is the skill man left back 💙' },
+    { id: 2, title: 'Luka Modric is the best trivela ⚽' },
+    { id: 3, title: 'Cristiano Ronaldo is the world class volley ⚽' },
+  ]);
+
+  function handleTodoClick(todo) {
+    console.log(todo);
+    const index = todoList.findIndex(x => x.id === todo.id);
+    if (index < 0) { return; }
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+  }
   return (
     <div className="App">
-      <h1>Welcome React Hooks</h1>
-      <ColorBox />
+      <h1> React hooks - TodoList</h1>
+      <ToDoList todos={todoList} onToDoClick={handleTodoClick} />
     </div>
   );
 }
---
+++ src/components/ToDoList/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+ToDoList.propTypes = {
+    todos: PropTypes.array,
+    onToDoClick: PropTypes.func,
+};
+
+ToDoList.defaultProps = {
+    todos: [],
+    onToDoClick: null,
+};
+function ToDoList(props) {
+    const { todos, onToDoClick } = props;
+
+    function handleClick(todo) {
+        if (onToDoClick) {
+            onToDoClick(todo);
+        }
+    }
+    return (
+        <ul className="to-do-list">
+            {todos.map(todo => (
+                <li key={todo.id}
+                    onClick={() => handleClick(todo)}
+                >
+                    {todo.title}
+                </li>
+            ))}
+        </ul>
+    );
+}
+
+export default ToDoList;
